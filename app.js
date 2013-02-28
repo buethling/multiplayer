2086a41758f92cb4fd55fac417dc525b629bb318
Chris Lydon <chris.lydon@gmail.com>
2013-02-27 21:35:15 -0500

    transition to express and players are removed when disconnected

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server)
+  , HashMap = require('hashmap').HashMap;
 
 // assuming io is the Socket.IO server object
 io.configure(function () { 
   io.set('log level', 1);
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+  //io.set("transports", ["xhr-polling"]); 
+  //io.set("polling duration", 10); 
 });
 
 var port = process.env.PORT || 80;
-app.listen(port);
+server.listen(port);
 
-// server up the index page
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+app.get('/hashmap.js', function(req, res) {
+  res.sendfile(__dirname + '/node_modules/hashmap/hashmap.js');
+});
 
 // holds the rectangles currently on the screen
-var rectangles = [],
-    count = 0;
+var rectangles = new HashMap();
 
 // handles socket stuff
 io.sockets.on('connection', function (socket) {
   // init
-  newRect = {'id': count, 'loc': {'x': 100, 'y': 100} };
-  socket.emit('init', { 'newRect': newRect, 'rectangles': rectangles });
-  rectangles[count] = newRect;
-  count++;
-  
+  newRect = {'id': socket.id, 'loc': {'x': 100, 'y': 100} };
+  socket.emit('init', { 'newRect': newRect, 'rectangles': rectangles._data });
+  rectangles.set(socket.id, newRect);
+  socket.broadcast.emit('update', rectangles._data);
+
   socket.on('move', function(data) {
-    rectangles[data.id] = data;
-    socket.broadcast.emit('update', rectangles);
+    rectangles.set(socket.id, data);
+    socket.broadcast.emit('update', rectangles._data);
+  });
+
+  socket.on('disconnect', function () {
+    rectangles.remove(socket.id);
+    io.sockets.emit('update', rectangles._data);
   });
 });
